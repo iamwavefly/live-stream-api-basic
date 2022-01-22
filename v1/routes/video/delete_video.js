0061bb4e3769eee7fce1f06906681aae73846dee
bluecodetechnology <hello@bluecodetech.co>
2022-01-22 23:16:56 +0100

    22/01/2021

--- v1/routes/video/delete_video.js
+++ v1/routes/video/delete_video.js
@@ -39,7 +39,7 @@ module.exports = function (app) {
                     videoExists = Array.isArray(videoExists)? videoExists[0] : videoExists;
                     userExists = Array.isArray(userExists)? userExists[0] : userExists;
 
-                    VIDEO.findOneAndRemove({token: request.body.token, video_id: request.body.video_id });
+                    VIDEO.deleteOne({token: request.body.token, video_id: request.body.video_id });
 
                     payload["is_verified"] = functions.stringToBoolean(userExists.is_verified)
                     payload["is_blocked"] = functions.stringToBoolean(userExists.is_blocked)
